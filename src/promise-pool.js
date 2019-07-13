f62334fb6c90710e36d03b73ac5ac15ceb89dd2a
Marcus Poehls <marcus.poehls@gmail.com>
2019-07-13 07:02:43 +0200

    keep a clean interface: move execution parts to promise pool executor

--- src/promise-pool.js
+++ src/promise-pool.js
@@ -1,9 +1,8 @@
 'use strict'
 
-const Deferred = require('p-defer')
-const EventEmitter = require('events')
+const PromisePoolExecutor = require('./promise-pool-executor')
 
-class PromisePool extends EventEmitter {
+class PromisePool {
   /**
    * Instantiates a new promise pool with a default
    * `concurrency: 10` and `items: []`.
@@ -11,12 +10,6 @@ class PromisePool extends EventEmitter {
    * @param {Object} options
    */
   constructor ({ concurrency = 10, items = [] } = {}) {
-    super()
-
-    this._active = 0
-    this._errors = []
-    this._results = []
-    this._pending = []
     this._items = items
     this._concurrency = concurrency
   }
@@ -63,159 +56,10 @@ class PromisePool extends EventEmitter {
       throw new Error('The first parameter for the .process(fn) method must be a function')
     }
 
-    for (const item of this._items) {
-      if (this._hasReachedConcurrencyLimit()) {
-        await this._processingSlot()
-      }
-
-      this._handle(callback, item)
-    }
-
-    return this._drained()
-  }
-
-  /**
-   * Creates a deferred promise and pushes the related
-   * callback to the pending queue. Returns the
-   * promise which is used to wait for the callback.
-   *
-   * @returns {Promise}
-   */
-  async _processingSlot () {
-    const { resolve, promise } = Deferred()
-    this._pending.push(resolve)
-
-    return promise
-  }
-
-  /**
-   * Asynchronously call the given processing function
-   * and pass the current `item` as an argument.
-   *
-   * @param {Function} callback
-   * @param {*} item
-   */
-  async _handle (callback, item) {
-    this._increaseActive()
-
-    try {
-      this._results.push(
-        await callback(item)
-      )
-    } catch (error) {
-      this._errors.push(error)
-    }
-
-    this._startNext()
-  }
-
-  /**
-   * Starts the next task from the list of pending
-   * tasks. Once all tasks finished processing,
-   * it emits the `pool:all-finished` event.
-   */
-  _startNext () {
-    this._decreaseActive()
-
-    if (this._hasPending()) {
-      const resolve = this._pending.shift()
-
-      return resolve()
-    }
-
-    if (this._allFinished()) {
-      this.emit('pool:all-finished')
-    }
-  }
-
-  /**
-   * Wait for all active tasks to finish. Once all
-   * the tasks finished processing, returns an
-   * object containing the results and errors.
-   *
-   * @returns {Object}
-   */
-  async _drained () {
-    if (this._hasActiveTasks()) {
-      const { resolve, promise } = Deferred()
-      this.once('pool:all-finished', resolve)
-      await promise
-    }
-
-    return {
-      errors: this._errors,
-      results: this._results
-    }
-  }
-
-  /**
-   * Determines whether the number of active tasks
-   * is greater or equal to the concurrency limit.
-   *
-   * @returns {Boolean}
-   */
-  _hasReachedConcurrencyLimit () {
-    return this._active >= this._concurrency
-  }
-
-  /**
-   * Increases the number of active tasks by one.
-   */
-  _increaseActive () {
-    ++this._active
-  }
-
-  /**
-   * Decreases the number of active tasks by one.
-   */
-  _decreaseActive () {
-    --this._active
-  }
-
-  /**
-   * Determines whether there are still pending or
-   * active tasks remaining for processing.
-   *
-   * @returns {Boolean}
-   */
-  _allFinished () {
-    return this._hasNoActiveTasks() && this._hasNoPendingTasks()
-  }
-
-  /**
-   * Determines whether there are active tasks.
-   *
-   * @returns {Boolean}
-   */
-  _hasActiveTasks () {
-    return this._active > 0
-  }
-
-  /**
-   * Determines whether there are no active tasks.
-   *
-   * @returns {Boolean}
-   */
-  _hasNoActiveTasks () {
-    return !this._hasActiveTasks()
-  }
-
-  /**
-   * Determines whether there are pending tasks.
-   *
-   * @returns {Boolean}
-   */
-  _hasPending () {
-    return this._pending.length > 0
-  }
-
-  /**
-   * Determines whether there are no pending tasks.
-   *
-   * @returns {Boolean}
-   */
-  _hasNoPendingTasks () {
-    return !this._hasPending()
+    return new PromisePoolExecutor({
+      items: this._items,
+      concurrency: this._concurrency
+    }).process(callback)
   }
 }
 
