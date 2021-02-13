573f31d45da12b8203130ead00144366e4babc81
Marcus Pöhls <marcus.poehls@gmail.com>
2021-02-13 17:08:47 +0100

    Stoppable

---
+++ src/stoppable.ts
@@ -0,0 +1,9 @@
+'use strict'
+
+export interface Stoppable {
+  /**
+   * Stop the promise pool and returns any results that already have been calculated.
+   * Stopping the pool waits for  active task to finish processing before returning.
+   */
+  stop (): void
+}
