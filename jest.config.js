963e4b18856bb2dbe88cd65a5947c3af27570180
Marcus Pöhls <marcus.poehls@gmail.com>
2021-03-27 18:11:32 +0100

    testEnvironment: 'node'

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@
 
 module.exports = {
   collectCoverage: true,
+  testEnvironment: 'node',
   coverageReporters: ['text', 'html'],
   setupFilesAfterEnv: ['jest-extended'],
   testMatch: ['**/test/**/*.[jt]s?(x)']
