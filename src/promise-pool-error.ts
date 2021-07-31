59d14c329a5962f5969341d3c94c6ad3ade9f944
Azrideus <mehrdadtavangar@gmail.com>
2021-07-31 18:09:12 +0300

    🚨 fix eslint rules

--- src/promise-pool-error.ts
+++ src/promise-pool-error.ts
@@ -1,59 +1,59 @@
-'use strict'
+"use strict";
 
 export class PromisePoolError<T> extends Error {
-  /**
-   * Returns the item that caused this error.
-   */
-  public item: T
+	/**
+	 * Returns the item that caused this error.
+	 */
+	public item: T;
 
-  /**
-   * Create a new instance for the given `message` and `item`.
-   *
-   * @param error  The original error
-   * @param item   The item causing the error
-   */
-  constructor (error: any, item: T) {
-    super()
+	/**
+	 * Create a new instance for the given `message` and `item`.
+	 *
+	 * @param error  The original error
+	 * @param item   The item causing the error
+	 */
+	constructor(error: any, item: T) {
+		super();
 
-    this.item = item
-    this.name = this.constructor.name
-    this.message = this.messageFrom(error)
+		this.item = item;
+		this.name = this.constructor.name;
+		this.message = this.messageFrom(error);
 
-    Error.captureStackTrace(this, this.constructor)
-  }
+		Error.captureStackTrace(this, this.constructor);
+	}
 
-  /**
-   * Returns a new promise pool error instance wrapping the `error` and `item`.
-   *
-   * @param {*} error
-   * @param {*} item
-   *
-   * @returns {PromisePoolError}
-   */
-  static createFrom<T>(error: any, item: T): PromisePoolError<T> {
-    return new this(error, item)
-  }
+	/**
+	 * Returns a new promise pool error instance wrapping the `error` and `item`.
+	 *
+	 * @param {*} error
+	 * @param {*} item
+	 *
+	 * @returns {PromisePoolError}
+	 */
+	static createFrom<T>(error: any, item: T): PromisePoolError<T> {
+		return new this(error, item);
+	}
 
-  /**
-   * Returns the error message from the given `error`.
-   *
-   * @param {*} error
-   *
-   * @returns {String}
-   */
-  private messageFrom (error: any): string {
-    if (error instanceof Error) {
-      return error.message
-    }
+	/**
+	 * Returns the error message from the given `error`.
+	 *
+	 * @param {*} error
+	 *
+	 * @returns {String}
+	 */
+	private messageFrom(error: any): string {
+		if (error instanceof Error) {
+			return error.message;
+		}
 
-    if (typeof error === 'object') {
-      return error.message
-    }
+		if (typeof error === "object") {
+			return error.message;
+		}
 
-    if (typeof error === 'string' || typeof error === 'number') {
-      return error.toString()
-    }
+		if (typeof error === "string" || typeof error === "number") {
+			return error.toString();
+		}
 
-    return ''
-  }
+		return "";
+	}
 }
--- src/promise-pool.ts
+++ src/promise-pool.ts
@@ -1,108 +1,108 @@
-'use strict'
+"use strict";
 
-import { ReturnValue } from './return-value'
-import { PromisePoolExecutor } from './promise-pool-executor'
+import { PromisePoolExecutor } from "./promise-pool-executor";
+import { ReturnValue } from "./return-value";
 
 export class PromisePool<T> {
-  /**
-   * The processable items.
-   */
-  private readonly items: T[]
+	/**
+	 * The processable items.
+	 */
+	private readonly items: T[];
 
-  /**
-   * The number of promises running concurrently.
-   */
-  private concurrency: number
+	/**
+	 * The number of promises running concurrently.
+	 */
+	private concurrency: number;
 
-  /**
-   * The error handler callback function
-   */
-  private errorHandler?: (error: Error, item: T) => void | Promise<void>
+	/**
+	 * The error handler callback function
+	 */
+	private errorHandler?: (error: Error, item: T) => void | Promise<void>;
 
-  /**
-   * Instantiates a new promise pool with a default `concurrency: 10` and `items: []`.
-   *
-   * @param {Object} options
-   */
-  constructor (items?: T[]) {
-    this.concurrency = 10
-    this.items = items ?? []
-    this.errorHandler = undefined
-  }
+	/**
+	 * Instantiates a new promise pool with a default `concurrency: 10` and `items: []`.
+	 *
+	 * @param {Object} options
+	 */
+	constructor(items?: T[]) {
+		this.concurrency = 10;
+		this.items = items ?? [];
+		this.errorHandler = undefined;
+	}
 
-  /**
-   * Set the number of tasks to process concurrently in the promise pool.
-   *
-   * @param {Integer} concurrency
-   *
-   * @returns {PromisePool}
-   */
-  withConcurrency (concurrency: number): PromisePool<T> {
-    this.concurrency = concurrency
+	/**
+	 * Set the number of tasks to process concurrently in the promise pool.
+	 *
+	 * @param {Integer} concurrency
+	 *
+	 * @returns {PromisePool}
+	 */
+	withConcurrency(concurrency: number): PromisePool<T> {
+		this.concurrency = concurrency;
 
-    return this
-  }
+		return this;
+	}
 
-  /**
-   * Set the number of tasks to process concurrently in the promise pool.
-   *
-   * @param {Number} concurrency
-   *
-   * @returns {PromisePool}
-   */
-  static withConcurrency (concurrency: number): PromisePool<unknown> {
-    return new this().withConcurrency(concurrency)
-  }
+	/**
+	 * Set the number of tasks to process concurrently in the promise pool.
+	 *
+	 * @param {Number} concurrency
+	 *
+	 * @returns {PromisePool}
+	 */
+	static withConcurrency(concurrency: number): PromisePool<unknown> {
+		return new this().withConcurrency(concurrency);
+	}
 
-  /**
-   * Set the items to be processed in the promise pool.
-   *
-   * @param {Array} items
-   *
-   * @returns {PromisePool}
-   */
-  for<T> (items: T[]): PromisePool<T> {
-    return new PromisePool<T>(items).withConcurrency(this.concurrency)
-  }
+	/**
+	 * Set the items to be processed in the promise pool.
+	 *
+	 * @param {Array} items
+	 *
+	 * @returns {PromisePool}
+	 */
+	for<T>(items: T[]): PromisePool<T> {
+		return new PromisePool<T>(items).withConcurrency(this.concurrency);
+	}
 
-  /**
-   * Set the items to be processed in the promise pool.
-   *
-   * @param {Array} items
-   *
-   * @returns {PromisePool}
-   */
-  static for<T> (items: T[]): PromisePool<T> {
-    return new this<T>().for(items)
-  }
+	/**
+	 * Set the items to be processed in the promise pool.
+	 *
+	 * @param {Array} items
+	 *
+	 * @returns {PromisePool}
+	 */
+	static for<T>(items: T[]): PromisePool<T> {
+		return new this<T>().for(items);
+	}
 
-  /**
-   * Set the error handler function to execute when an error occurs.
-   *
-   * @param {Function} handler
-   *
-   * @returns {PromisePool}
-   */
-  handleError (handler: (error: Error, item: T) => Promise<void> | void): PromisePool<T> {
-    this.errorHandler = handler
+	/**
+	 * Set the error handler function to execute when an error occurs.
+	 *
+	 * @param {Function} handler
+	 *
+	 * @returns {PromisePool}
+	 */
+	handleError(handler: (error: Error, item: T) => Promise<void> | void): PromisePool<T> {
+		this.errorHandler = handler;
 
-    return this
-  }
+		return this;
+	}
 
-  /**
-   * Starts processing the promise pool by iterating over the items
-   * and running each item through the async `callback` function.
-   *
-   * @param {Function} The async processing function receiving each item from the `items` array.
-   *
-   * @returns Promise<{ results, errors }>
-   */
-  async process<R> (callback: (item: T) => R | Promise<R>): Promise<ReturnValue<T, R>> {
-    return new PromisePoolExecutor<T, R>()
-      .withConcurrency(this.concurrency)
-      .withHandler(callback)
-      .handleError(this.errorHandler)
-      .for(this.items)
-      .start()
-  }
+	/**
+	 * Starts processing the promise pool by iterating over the items
+	 * and running each item through the async `callback` function.
+	 *
+	 * @param {Function} The async processing function receiving each item from the `items` array.
+	 *
+	 * @returns Promise<{ results, errors }>
+	 */
+	async process<R>(callback: (item: T) => R | Promise<R>): Promise<ReturnValue<T, R>> {
+		return new PromisePoolExecutor<T, R>()
+			.withConcurrency(this.concurrency)
+			.withHandler(callback)
+			.handleError(this.errorHandler)
+			.for(this.items)
+			.start();
+	}
 }
