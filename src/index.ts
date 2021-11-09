0ae4dd0913c9977bbb30bfedc2da91cc734c70e8
Marcus Pöhls <mp@seda.digital>
2021-11-09 13:42:54 +0100

    exports

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,10 @@
 
 import { PromisePool } from './promise-pool'
 
-export { PromisePool }
+export default PromisePool
+
+export * from './promise-pool'
+export * from './promise-pool-error'
+export * from './return-value'
+export * from './stoppable'
+export * from './stop-the-promise-pool-error'
