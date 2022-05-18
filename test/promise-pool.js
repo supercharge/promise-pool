bc6607e0e4b637d4c963974ed8a8ed6faba059f9
Marcus Pöhls <mp@seda.digital>
2022-05-18 05:53:31 +0200

    bump dependencies

--- test/promise-pool.js
+++ test/promise-pool.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const { test } = require('uvu')
-const expect = require('expect')
+const { expect } = require('expect')
 const { PromisePool } = require('../dist')
 
 const pause = timeout => new Promise(resolve => setTimeout(resolve, timeout))
